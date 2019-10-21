06e0e23dd741467eb9f52d43919bf5b66fc4d316
Pavel Belyakov <pavel.beliakov@itechart-group.com>
2019-10-21 13:01:37 +0300

    translating old repo to legacy

--- README.md
+++
@@ -1,1 +0,0 @@
-# react-native-saga-template
--- src/actions/map.js
+++
@@ -1,7 +0,0 @@
-import {navigate} from 'src/navigation/navigationService';
-import routes from 'src/navigation/routes';
-
-const map = {
-  14: () => navigate(routes.app),
-};
-export default map;
