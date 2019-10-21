c122b7ac3bea5ef5a2bbdf3132c15296c467dab3
Pavel Belyakov <pavel.beliakov@itechart-group.com>
2019-10-21 16:35:06 +0300

    reverting master to pure saga project without firebase

--- src/app.js
+++ src/app.js
@@ -1,32 +1,10 @@
 import React, {Component} from 'react';
 import {SafeAreaView} from 'react-native';
-import firebase from 'react-native-firebase';
 
 import {setNavigator} from './navigation/navigationService';
 import Navigator from './navigation';
 
 export default class AppComponent extends Component {
-  componentDidMount() {
-    //console.log(firebase, firebase.auth());
-    firebase
-      .messaging()
-      .getToken()
-      .then(fcmToken => {
-        console.log(fcmToken);
-      });
-    setNavigator(this.navigator);
-
-    firebase
-      .auth()
-      .signInWithEmailAndPassword('test@test.com', 'password')
-      .then(console.log)
-      .catch(function(error) {
-        // Handle Errors here.
-        console.log(error);
-        // ...
-      });
-  }
-
   render() {
     return (
       <SafeAreaView style={{flex: 1}}>
